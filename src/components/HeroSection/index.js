acb69b1b141a4fb436a635e8ce50beb030f05b38
AKSHAY <akshayjadhav461999@gmail.com>
2020-11-22 13:53:50 +0530

    hero section done

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import video from "../../assests/video.mp4";
+import { Button } from "../Button/ButtonElement";
+import {
+  ArrowForward,
+  ArrowRight,
+  HeroBG,
+  HeroBtnWrapper,
+  HeroContainer,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  VideoBG,
+} from "./HeroSectionElements";
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer>
+      <HeroBG>
+        <VideoBG autoPlay loop muted src={video} type="video/mp4" />
+      </HeroBG>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>Sign Up for today and get free 200 AKS coins</HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
