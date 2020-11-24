4c4beca1f96bb4b5ad011c2918c1ac4cf57662f0
AKSHAY <akshayjadhav461999@gmail.com>
2020-11-24 14:23:10 +0530

    Route added to sigin page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" exact component={SignIn} />
+      </Switch>
     </Router>
   );
 }
